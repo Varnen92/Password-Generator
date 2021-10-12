e3500358f7dcf7e3ffdb94dddb498293062baae2
Robert Sandoval <robsand1992@yahoo.com>
2021-10-11 17:32:10 -0700

    Created generate Password function to check length vs required length prompt

--- Assets/script.js
+++ Assets/script.js
@@ -9,13 +9,8 @@
 const alphaLower = "abcdefghijklmnopqrstuvwxyz";
 const alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "0123456789";
-const symbols = "!@#$%^&*_-+=";
+const symbols = "!@#$%&*";
 
-var lowercasePrompt;
-var uppercasePrompt;
-var numericalPrompt;
-var specialcharacterPrompt;
-var length;
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -31,85 +26,90 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Length of password Function
-var lengthPassword = function(){
-  length = window.prompt("Please confirm the length of your password! Minimum characters are 8, max are 128!");
-  length = parseInt(length);
-    if (length >=8 && length <= 128) {
-      window.alert("Your password length will be " + length + "!");
-    }
-    else {
-      window.alert("Please enter a valid password length!")
-      return lengthPassword();
-    }
-  };
-
-// Lowercase function
-var lowercasePassword = function(){
-  lowercasePrompt = window.confirm("Do you want to include lower case characters in your password?");
-  if (lowercasePrompt) {
-    window.alert ("Lowercase characters will be included in your password!");
-  }
-  else {
-    window.alert ("Lower characters will NOT be included in your password!");
-  };
-};
-// Uppercase function
-var uppercasePassword = function(){
-  uppercasePrompt = window.confirm("Do you want to include upper case characters in your password?");
-  if (uppercasePrompt) {
-    window.alert ("Uppercase characters will be included in your password!");
-    }
-  else {
-    window.alert ("Uppercase characters will NOT be included in your password!");
-  }
-};
-// Numerical function
-
-var numericalPassword = function(){
-  numericalPrompt = window.confirm("Do you want to include numbers in your password?");
-  if (numericalPrompt){
-    window.alert ("Numbers will be included in your password!"); 
-  }
-  else {
-    window.alert ("Numbers will NOT be included in your password!");
-  }
-};
-// Special Character function
-var specialcharacterPassword = function(){
-  specialcharacterPrompt = window.confirm("Do you want us to include special characters in your password?");
- if (specialcharacterPrompt){
-   window.alert ("Special characters will be included in your password!");
- } 
- else {
-   window.alert ("Special characters will NOT be included in your password!");
- }
-};
+// Random alpha lower function
+/* function getRandomAlphaLower() {
+  alphaLower[Math.floor(Math.random() * alphaLower.length)];
+}
+ */
 
 // Generate Password function upon Button Click
-function generatePassword (){
-   // First prompt asking for length of password
-  lengthPassword();
-  // Check if user wants lower case
-  lowercasePassword();
-  // Check if user wants upper case
-  uppercasePassword();
-  // Check if user wants numerical prompts included
-  numericalPassword();
-  // Check if user wants special characters included
-  specialcharacterPassword(); 
+function generatePasswordOptions() {
+  // Length of password Function
+  var lengthPassword = window.prompt("Please confirm the length of your password! Minimum characters are 8, max are 128!");
+  lengthPassword = parseInt(lengthPassword);
+  if (lengthPassword >= 8 && lengthPassword <= 128) {
+    window.alert("Your password length will be " + lengthPassword + "!");
+  }
+  else {
+    window.alert("Please enter a valid password length!")
+    return lengthPassword();
+  };
 
-  if (lowercasePrompt || uppercasePrompt || numericalPrompt || specialcharacterPrompt){
+  // Lowercase function
+  var lowercasePassword = window.confirm("Do you want to include lower case characters in your password?");
+  if (lowercasePassword) {
+    window.alert("Lowercase characters will be included in your password!");
+  }
+  else {
+    window.alert("Lower characters will NOT be included in your password!");
+  };
+  // Uppercase function
+  var uppercasePassword = window.confirm("Do you want to include upper case characters in your password?");
+  if (uppercasePassword) {
+    window.alert("Uppercase characters will be included in your password!");
+  }
+  else {
+    window.alert("Uppercase characters will NOT be included in your password!");
+  };
+  // Numerical function
+
+  var numericalPassword = window.confirm("Do you want to include numbers in your password?");
+  if (numericalPassword) {
+    window.alert("Numbers will be included in your password!");
+  }
+  else {
+    window.alert("Numbers will NOT be included in your password!");
+  };
+  // Special Character function
+  var specialcharacterPassword = window.confirm("Do you want us to include special characters in your password?");
+  if (specialcharacterPassword) {
+    window.alert("Special characters will be included in your password!");
+  }
+  else {
+    window.alert("Special characters will NOT be included in your password!");
+  };
+
+  if (lowercasePassword || uppercasePassword || numericalPassword || specialcharacterPassword) {
     window.alert("Your password will include the criteria you selected!");
   }
-  
-  else { 
-    window.alert("You did not select ANY password type to be included, please repeat from the start and select at LEAST one! Thank you!");
-    generatePassword ();
-  }; 
 
+  else {
+    window.alert("You did not select ANY password type to be included, please repeat from the start and select at LEAST one! Thank you!");
+    generatePasswordOptions();
+  };
+
+  var passwordCriteria = {
+    length: lengthPassword,
+    hasUppercase: uppercasePassword,
+    hasLowercase: lowercasePassword,
+    hasSpecial: specialcharacterPassword,
+    hasNumbers: numericalPassword,
+  };
+  return passwordCriteria;
 
 };
+
+function generatePassword() {
+  var passwordGenerateOptions = generatePasswordOptions();
+  var pCharacters = "";
+  while (passwordGenerateOptions.length != pCharacters.length){
+  if (passwordGenerateOptions.hasLowercase) {
+    pCharacters += alphaLower[Math.floor(Math.random() * alphaLower.length)];
+  }
+  window.alert(pCharacters);
+
+  }
+}
 
 
 // Uppercase Function with Switch Prompts
